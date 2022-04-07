5dcb299d6cfd7ba73503e759b004ce66fcfac6c7
SyedAmeenHussaini <ameenhussaini0@gmail.com>
2022-04-08 02:29:21 +0530

    DAY2 completed

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -9,8 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-    final Profile_url =
-       "https://pbs.twimg.com/profile_images/1247651655065075712/fszwVtCL_400x400.jpg";
+  final Profile_url =
+      "https://pbs.twimg.com/profile_images/1247651655065075712/fszwVtCL_400x400.jpg";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,9 +32,31 @@ class _HomePageState extends State<HomePage> {
           VxBox()
               .square(100)
               .roundedFull
-              .neumorphic(color: Vx.purple500, elevation: 30.0).
-              bgImage(DecorationImage(image: NetworkImage(Profile_url)))
+              .neumorphic(color: Vx.purple500, elevation: 30.0)
+              .bgImage(DecorationImage(image: NetworkImage(Profile_url)))
+              .make(),
+          "Hi "
+              .richText
+              .withTextSpanChildren(["Miracle".textSpan.bold.make()])
+              .white
+              .xl3
               .make()
+              .p12(),
+          "Solo Traveler".text.white.bold.xl.make(),
+           VxTextField(
+            borderType: VxTextFieldBorderType.none,
+            borderRadius: 18,
+            hint: "Searh",
+            fillColor: Vx.gray200.withOpacity(0.3),
+            contentPaddingTop: 14,
+            autofocus: false,
+            prefixIcon: Icon(Icons.search_off_outlined, color:Colors.white),
+
+          ).customTheme(themeData: 
+          ThemeData(
+            brightness:
+            Brightness.dark)).cornerRadius(10).p16()
+
         ].column())
             .makeCentered()
       ]),
